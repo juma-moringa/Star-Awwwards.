cf7c807bc3c1c88d049f4146a838187f37199fe8
juma-moringa <allan.juma@student.moringaschool.com>
2021-07-16 19:32:26 +0300

    add database configurations in the settings.py

--- starproject/views.py
+++ starproject/views.py
@@ -1,8 +1,10 @@
 from starproject.forms import SignUpForm
 from django.shortcuts import render,redirect
 from django.contrib.auth import authenticate, login
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+@login_required(login_url='/accounts/login/')
 def index(request):
     return render( request,'index.html')
 
