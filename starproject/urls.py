bbc87fbd3b903c5667e14c283aef2b22caf09274
juma-moringa <allan.juma@student.moringaschool.com>
2021-07-16 14:20:24 +0300

    add the index view function and url route

---
+++ starproject/urls.py
@@ -0,0 +1,11 @@
+from django.conf.urls import url
+from . import views
+from django.conf.urls.static import static
+from django.contrib.auth import views as auth_views
+from django.conf import settings
+
+urlpatterns=[
+    url(r'^$',views.index, name='index'),
+   
+    
+]
