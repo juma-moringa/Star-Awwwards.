0b44fa9d6ed6c41a396c8f0f3c0340cfcb3da6c1
juma-moringa <allan.juma@student.moringaschool.com>
2021-07-16 22:18:07 +0300

    add profile and projects model class

--- starproject/models.py
+++ starproject/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from cloudinary.models import CloudinaryField
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="profile",primary_key=True)
+    profile_picture = CloudinaryField('image')
+    bio= models.TextField()
+    contact = models.CharField(max_length=60,blank=True)
+    
+
+
+
+class Project(models.Model):
+    title = models.CharField(max_length=60,blank=True)
+    image = CloudinaryField('image')
+    description = models.TextField()
+    link = models.URLField(blank=True)
+    profile = models.ForeignKey(Profile,null=True,on_delete=models.CASCADE)
+   
