4f9bc9309669c26eaafd90dad7c7ff4a1336fdf6
juma-moringa <allan.juma@student.moringaschool.com>
2021-07-16 22:35:54 +0300

    add rewiews model class to enable ratings of projects

--- starproject/models.py
+++ starproject/models.py
@@ -18,4 +18,22 @@ class Project(models.Model):
     description = models.TextField()
     link = models.URLField(blank=True)
     profile = models.ForeignKey(Profile,null=True,on_delete=models.CASCADE)
+
+
+
+
+class Review(models.Model):
+    ratings= (
+        (1, '1'),(2, '2'),
+        (3, '3'),(4, '4'),
+        (5, '5'),(6, '6'),
+        (7, '7'),(8, '8'),
+        (9, '9'),(10, '10'),
+    )
+    user = models.ForeignKey(User,null=True,blank=True)
+    content = models.IntegerField(choices=ratings,blank=False,default=0)
+    design = models.IntegerField(choices=ratings,default=0,blank=False)
+    usability = models.IntegerField(choices=ratings,blank=False,default=0)
+    project = models.ForeignKey(Project,null=True,on_delete=models.CASCADE)
+    average =  models.DecimalField(default=1,blank=False,max_digits=30,decimal_places=2)
    
