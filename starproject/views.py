33d4bf946c16b8382fe9b1663c67f452f16fdd6c
juma-moringa <allan.juma@student.moringaschool.com>
2021-07-16 18:53:56 +0300

    define the register function to render the signup form as a request

--- starproject/views.py
+++ starproject/views.py
@@ -1,7 +1,21 @@
-from django.shortcuts import render
+from starproject.forms import SignUpForm
+from django.shortcuts import render,redirect
+from django.contrib.auth import authenticate, login
 
 # Create your views here.
 def index(request):
     return render( request,'index.html')
 
-    
+def register(request):
+    if request.method=="POST":
+        form=SignUpForm(request.POST) 
+        if form.is_valid():
+           form.save()
+           username = form.cleaned_data.get('username')
+           user_password = form.cleaned_data.get('password1')
+           user = authenticate(username=username, password=user_password)
+           login(request, user)
+        return redirect('login')
+    else:
+        form= SignUpForm()
+    return render(request, 'registration/registration_form.html', {"form":form})        
